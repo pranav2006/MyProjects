d37370d540668c6e54cad8cb9902ca0e3bf0f7da
pranav2006 <83268509+pranav2006@users.noreply.github.com>
2025-04-01 21:07:17 +0530

    changes

--- AI/Object Classification/backend/main.py
+++ AI/Object Classification/backend/main.py
@@ -53,5 +53,5 @@ async def predict(file: UploadFile = File(...)):
     except Exception as e:
         return JSONResponse({"error": str(e)}, status_code=500)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+#    uvicorn.run(app, host="0.0.0.0", port=8000)
